12fbc6e787251f33bff99af30273e01ff8d324ca
Rohit Chand <chandrohitsumit@gmail.com>
2024-11-13 15:18:46 +0530

    Reinstall dependencies and remove old lock file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
+// Export Vite config
 export default defineConfig({
+  mode: 'production', // This explicitly sets production mode for builds
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
